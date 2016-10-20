79baccc8870665dda7004689c865241d89b7bcc1
ccflack <ccflack@me.com>
2016-10-20 13:30:04 -0400

    Made all html image tags into rails image_tag

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+
+  def index
+    @message = "Test for visibility."
+
+  end
+
+  def posts
+
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'posts#posts' as: "posts"
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
